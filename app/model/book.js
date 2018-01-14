224f71e4b0444b4557309e64445ceaeab95d36a4
oliverlai55 <oliverlai55@gmail.com>
2018-01-13 20:29:37 -0500

    Add bookschema

---
+++ app/model/book.js
@@ -0,0 +1,29 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+// book schema definition
+let BookSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		author: { type: String, required: true },
+		year: { type: Number, required: true },
+		pages: { type: Number, required: true, min: 1 },
+		createdAt: { type: Date, default: date.now },
+	},
+	{
+		versionKey: false,
+	},
+);
+
+// Sets the createdAt paramter equal to the current time
+BookSchema.pre('save', next => {
+	now = new Date();
+	if (!this.createdAt) {
+		this.createdAt = now;
+	}
+	next();
+});
+
+// Exports the BookSchema for use elsewhere
+module.exports = mongoose.modal('book', BookSchema);
+
