338a8367f73fb784040a2f29658b1a5d4cf3bca1
oliverlai55 <oliverlai55@gmail.com>
2018-01-13 21:06:40 -0500

    Set up routes

---
+++ app/routes/book.js
@@ -0,0 +1,57 @@
+let mongoose = require('mongoose');
+let Book = require('../models/book');
+
+/*
+ * GET /book route to retrieve all the books.
+ */
+function getBooks(req, res) {
+	// Query the DB and if no errors, send all the books
+	let query = Book.find({});
+	query.exec((err, books) => {
+		if (err) res.send(err);
+		// If no errors, send them back to the client
+		res.json(books);
+	});
+}
+
+/*
+ * POST /book to save a new book.
+ */
+function postBook(req, res) {
+	// Create a new book
+	var newBook = new Book(req.body);
+	// Save it in the DB
+	newBook.save((err, book) => {
+		if (err) {
+			res.send(err);
+		} else {
+			// if no errors, send it back to the client
+			res.json({ message: 'Book successfully added!', book });
+		}
+	});
+}
+
+/*
+ * DELETE /book/:id to delete a book given its id.
+ */
+function deleteBook(req, res) {
+	Book.remove({ _id: req.params.id }, (err, result) => {
+		res.json({ message: 'Book successfully deleted', result });
+	});
+}
+
+/*
+ * PUT /book/:id to updatea a book given its id
+ */
+function updateBook(req, res) {
+	Book.findById({ _id: req.params.id }, (err, book) => {
+		if (err) res.send(err);
+		Object.assign(book, req.body).save((err, book) => {
+			if (err) res.send(err);
+			res.json({ message: 'Book updated', book });
+		});
+	});
+}
+
+// export all the functions
+module.exports = { getBooks, postBook, getBook, deleteBook, updateBook };
